3ca2e909e2f19bf10cdbc62ded1ab0fe3c1dfa96
Artem Dadychenko <artem_dadychenko@mail.ru>
2018-03-11 23:24:54 +0300

    need a review freebeeOrganizers model

---
+++ app/models/freebeeOrganizers.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+mongoose.Promise = Promise;
+
+const freebeeOrganizers = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  type: {
+    type: String,
+    required: true
+  },
+  location: { // or google id object
+    type: [Number],
+    index: {
+        type: "2dsphere",
+        sparse: true
+    }
+  },
+  details: {
+    contact_phone: {
+      type: String,
+      required: false,
+    },
+    address: {
+      type: String,
+      required: false,
+    }
+  }
+});
+
+module.exports = mongoose.model("freebeeOrganizers", freebeeOrganizers);
