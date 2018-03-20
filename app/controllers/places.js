7dabf1e3a128be2bf8debd901ab700941e0c06f2
George Puisha <stearis@tut.by>
2018-03-20 09:17:32 +0300

    Add missnig semicolon

--- app/controllers/places.js
+++ app/controllers/places.js
@@ -1,6 +1,6 @@
 const needle = require("needle");
 
-const url = "https://maps.googleapis.com/maps/api/place/details/json?placeid="
+const url = "https://maps.googleapis.com/maps/api/place/details/json?placeid=";
 
 module.exports.getPlaceById = (req, res) => {
     const requestUrl = url + req.params.placeId + "&key=" + process.env.GOOGLE_API_KEY;
