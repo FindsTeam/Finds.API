13a4c6370fcc1925f752c81cc81690586ff60da2
George Puisha <stearis@tut.by>
2018-03-20 09:01:11 +0300

    Remove console output

--- app/controllers/places.js
+++ app/controllers/places.js
@@ -5,10 +5,7 @@ const url = "https://maps.googleapis.com/maps/api/place/details/json?placeid="
 module.exports.getPlaceById = (req, res) => {
     const requestUrl = url + req.params.placeId + "&key=" + process.env.GOOGLE_API_KEY;
 
-    console.log(requestUrl);
-
     needle.get(requestUrl, (request, response) => {
         res.json(response.body);
     });
-
 }
