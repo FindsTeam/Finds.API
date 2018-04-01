7fe4d00abe6a837170d8801b2dc8c313b580d688
George Puisha <stearis@tut.by>
2018-04-02 00:12:47 +0300

    Fix domain and audience issues

--- api.js
+++ api.js
@@ -6,6 +6,8 @@ const routesApi = require("./app/routes/index");
 const bodyParser = require("body-parser");
 const express = require("express");
 
+const checkJwt = require('./app/middleware/jwt');
+
 const app = express();
 
 app.use(bodyParser.json());
@@ -17,6 +19,8 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+
+app.use(checkJwt);
 
 app.use("/api", routesApi);
 
--- app/middleware/jwt.js
+++ app/middleware/jwt.js
@@ -1,14 +1,16 @@
 const jwt = require('express-jwt');
-const jwksRsa = require('jwks-rsa');
+const jwks = require('jwks-rsa');
 
-module.exports.checkJwt = jwt({
+const checkJwt = jwt({
     secret: jwks.expressJwtSecret({
         cache: true,
         rateLimit: true,
         jwksRequestsPerMinute: 5,
-        jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
+        jwksUri: `${process.env.AUTH0_DOMAIN}.well-known/jwks.json`
     }),
     audience: process.env.AUTH0_API_IDENTIFIER,
     issuer: process.env.AUTH0_DOMAIN,
     algorithms: ['RS256']
 });
+
+module.exports = checkJwt;
