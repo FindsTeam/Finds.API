1c4f6a61b21586d48343a06bf40ad3079fb9309a
George Puisha <stearis@tut.by>
2018-04-15 20:08:04 +0300

    More allowed headers, fix audience field in jwt

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Access-Control-Request-Method, Access-Control-Request-Headers, Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
 });
 app.use("/api", routesApi);
--- app/middleware/jwt.js
+++ app/middleware/jwt.js
@@ -8,7 +8,7 @@ const checkJwt = jwt({
         jwksRequestsPerMinute: 5,
         jwksUri: `${process.env.AUTH0_DOMAIN}.well-known/jwks.json`
     }),
-    audience: process.env.AUTH0_API_IDENTIFIER,
+    aud: process.env.AUTH0_API_IDENTIFIER,
     issuer: process.env.AUTH0_DOMAIN,
     algorithms: ['RS256']
 });
