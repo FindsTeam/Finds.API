fb378026892f3aa59cbef649ce937a8ab9993942
George Puisha <stearis@tut.by>
2018-04-15 21:48:14 +0300

    Remove organizer becoming route

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -50,25 +50,3 @@ module.exports.updateUserById = (req, res) => {
         }
     });
 };
-
-module.exports.becomeOrganizer = (req, res) => {
-    Users.findById(req.params.id, (err, user) => {
-        if (err) {
-            if (err.kind === 'ObjectId') {
-                return res.json({ message: `Could not find a user with id ${req.params.id}` });
-            }
-            return res.json({ message: `An error occurred during the search` });
-        } else {
-            user.groups.push(groups.organizer);
-            user.phone = req.body.phone;
-            user.address = req.user.address;
-            user.save((error, data) => {
-                if (error) {
-                    return res.json({ message: `Can't update user "${data._id}"` });
-                } else {
-                    res.json(data);
-                }
-            });
-        }
-    });
-};
--- app/routes/index.js
+++ app/routes/index.js
@@ -8,21 +8,22 @@ const userController = require("../controllers/users");
 
 router.get("/login/:idToken", loginController.login);
 
+// users
 router.get("/user/id/:id", userController.getUserById);
 router.get("/user/name/:name", userController.getUserByName);
 router.put("/user/:id", userController.updateUserById);
 
-router.put("/user/evolve/:id", userController.becomeOrganizer);
-
-// TODO: Recent by id_token
+// markers CRUD
 router.post("/marker", markerController.createMarker);
 router.get("/marker/:id", markerController.getMarkerById);
 router.put("/marker/:id", markerController.updateMarkerById);
 router.delete("/marker/:id", markerController.deleteMarkerById);
+// markers + users
 router.get("/marker/:idToken/:amount", markerController.recentMarkersByIdToken);
-
+// markers + position
 router.get("/marker/near/:lat/:lng", markerController.getMarkersNear);
 
+// google
 router.get("/place/:placeId", placeController.getPlaceById);
 
 module.exports = router;
