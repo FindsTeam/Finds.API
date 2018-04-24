9c3f30d5399af166b77cabd74fb52425f9a3e213
George Puisha <stearis@tut.by>
2018-04-24 09:15:25 +0300

    Add and configure testing environment

---
+++ test/test-chat.js
@@ -0,0 +1,12 @@
+const chai = require("chai");
+const chatApi = require("../app/routes/chat");
+const should = chai.should();
+const expect = chai.expect;
+
+describe("Testing test system", () => {
+  describe("true === true", () => {
+      it("should return true", () => {
+          return (true === true);
+      });
+  });
+});
