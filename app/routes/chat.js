6c1a7caa2373563a56dffbbae4965c0d4ff3bf7d
George Puisha <stearis@tut.by>
2018-04-29 16:12:57 +0300

    Add multiple client test

--- app/routes/chat.js
+++ app/routes/chat.js
@@ -1,6 +1,34 @@
+const decode = require("jwt-decode");
+
+const Users = require("../models/users");
+
 module.exports = (socket) => {
-  socket.emit("connected");
-  socket.on("message", (message) => {
-    console.log("message: " + message);
+  socket.emit("check token");
+  socket.on("token", (token) => {
+    const { email, nickname } = decode(token);
+    Users.findOne({ email, nickname }, (err, user) => {
+      if (user) {
+        socket.emit("token is valid");
+
+        socket.on("subscribe", (room) => {
+          console.log("joining room", room);
+          socket.join(room);
+        });
+
+        socket.on("subscribe", (room) => {
+          socket.emit("subscribed");
+          console.log("joining room", room);
+          socket.join(room);
+        });
+
+        socket.on("send message", (data) => {
+          console.log("sending room post", data.room);
+          socket.broadcast.to(data.room).emit("conversation private post", {
+              message: data.message
+          });
+      });
+      }
+    });
   });
+
 }
--- test/test-chat.js
+++ test/test-chat.js
@@ -11,6 +11,8 @@ const options = {
   forceNew: true
 };
 
+const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ik1EWkZNVE00T0RSRE1UVTRSalV6TVVFNU9FTkZOREZDTWtWR1JVUkdOall5TTBNNE1UVXdSZyJ9.eyJuaWNrbmFtZSI6InRlc3QiLCJuYW1lIjoidGVzdEB0ZXN0LnRlc3QiLCJwaWN0dXJlIjoiaHR0cHM6Ly9zLmdyYXZhdGFyLmNvbS9hdmF0YXIvZGQ0NmE3NTZmYWFkNDcyN2ZiNjc5MzIwNzUxZjZkZWE_cz00ODAmcj1wZyZkPWh0dHBzJTNBJTJGJTJGY2RuLmF1dGgwLmNvbSUyRmF2YXRhcnMlMkZ0ZS5wbmciLCJ1cGRhdGVkX2F0IjoiMjAxOC0wNC0yM1QxOTo1MjozOS42MjVaIiwiZW1haWwiOiJ0ZXN0QHRlc3QudGVzdCIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiaXNzIjoiaHR0cHM6Ly9mcmVlYmVlLmV1LmF1dGgwLmNvbS8iLCJzdWIiOiJhdXRoMHw1YWRlMzk4NzdmNDdmZDYxNDFmMzI5NWIiLCJhdWQiOiJ1OFhJakxhelZ3enhyTDNsNGZqck15VFNDdU5sdmxNOCIsImlhdCI6MTUyNDUxMzE2NSwiZXhwIjoxNTI0NTQ5MTY1LCJhdF9oYXNoIjoiMVRQT19VM3RVXy1lNnRnT3I4ZXJGZyIsIm5vbmNlIjoieVVQQkp2d0Juc21SUDI2Qm93ZHZGcHlmdWRNWFllUDYifQ.SOVMFEdXd3jyYTiOJ3pTnu31sXYGjz9JO6JX820MEQvGNttfg-J0ka2qpoB-I3PpIzACMeN7hjiG9J1h2kibhCIkDFOg2UXWVeTaBPtn_Frj4Z3-AdTH-VeZ8bIK64uIKP7xZMJgujRKbezTcnymRA9MGJAze6lCywuJA_fYJFjsANo3JI_ydwsJ3fkLyyEEI0WtAAXjqW2rvcpRH4f3x-pS1cXVarAc0_ZkZWYSHRsLiZrAnIAxoMu7Wf2JnGQrAAS5wZuuo4MNKa78FVptUXLESwwJ2U6niEcWHF5xKaw6aehKyDAohoI9bTbfRqbe3E7aIjA2MI30x3RtwfoQTQ";
+
 describe("Testing test system", () => {
   describe("true === true", () => {
       it("should return true", () => {
@@ -19,7 +21,7 @@ describe("Testing test system", () => {
   });
 });
 
-describe("Testing chat API", () => {
+describe("Testing chat API with a single client", () => {
   var client;
 
   beforeEach(function(done) {
@@ -34,18 +36,95 @@ describe("Testing chat API", () => {
       console.log("disconnected...");
     });
   });
+  it("Client", () => {
+    it("should receive 'checkToken' event", (done) => {
+      client.on("check token", () => {
+        done();
+      });
+    });
 
-  it("should receive 'connected' event", (done) => {
-    client.on("connected", () => {
-      done();
+    it("should emit a token", (done) => {
+      client.on("check token", () => {
+        client.emit("token", token);
+        client.on("token is valid", () => {
+          done();
+        });
+      });
+    });
+
+    it("should emit a token", (done) => {
+      client.on("check token", () => {
+        client.emit("token", token);
+        client.on("token is valid", () => {
+          done();
+        });
+      });
+    });
+  });
+});
+
+describe("Testing chat API with a single client", () => {
+  var client;
+
+  beforeEach(function(done) {
+    clientOne = io.connect(socketUrl, options);
+    clientTwo = io.connect(socketUrl, options);
+
+    clientOne.on("connect", () => {
+      console.log("clientOne connected");
+      clientOne.on("check token", () => {
+        clientOne.emit("token", token);
+        clientOne.on("token is valid", () => {
+          console.log("clientOne token is valid");
+        });
+      });
+    });
+
+    clientTwo.on("connect", () => {
+      console.log("clientTwo connected");
+      clientTwo.on("check token", () => {
+        clientTwo.emit("token", token);
+        clientTwo.on("token is valid", () => {
+          console.log("clientTwo token is valid");
+          done();
+        });
+      });
+    });
+
+    clientOne.on("disconnect", () => {
+      console.log("clientOne disconnected");
+    });
+
+    clientTwo.on("disconnect", () => {
+      console.log("clientTwo disconnected");
     });
   });
 
-  it("should emit message", (done) => {
-    client.on("connected", () => {
-      const message = "Hello!"
-      client.emit("message", message);
-      done();
+  it("Clients", () => {
+    it("should join a room", (done) => {
+      const roomId = "randomid";
+
+      clientOne.emit("subscribe", conversation_id);
+      clientTwo.emit("subscribe", conversation_id);
+
+      clientOne.emit("send message", {
+        room: roomId,
+        message: "Some message"
+      });
+
+      clientOne.on("subscribed", () => {
+        clientTwo.emit("send message", {
+          room: roomId,
+          message: "Some message"
+        });
+
+        clientTwo.on("subscribed", () => {
+          done();
+        })
+      });
+
     });
   });
+
+  
 });
