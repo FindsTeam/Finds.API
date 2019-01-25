7ba349c4aa98bd0b7da92484b23eaaa9b75f2d7b
Artem Dadychenko <a.dadychenko@mail.freebee.by>
2019-01-25 18:33:08 +0300

    Rewrite create feedback

--- app/controllers/feedback.js
+++ app/controllers/feedback.js
@@ -1,24 +1,5 @@
 const Feedback = require('../models/feedback');
 
-const feedbackMapper = (body) => {
-  const feedback = new Feedback();
-
-  if (body) {
-    const {
-      address, type, description, author,
-    } = body;
-
-    feedback.address = address;
-    feedback.type = type;
-    feedback.creationDate = Date.now();
-    feedback.description = description || null;
-    feedback.author = author || null;
-
-    return feedback;
-  }
-  return null;
-};
-
 module.exports.getFeedback = (req, res) => {
   Feedback.find()
     .limit(100)
@@ -43,21 +24,31 @@ module.exports.getFeedbackById = (req, res) => {
 };
 
 module.exports.createFeedback = (req, res) => {
-  const { address, type } = req.body;
-  if (address && type && type.length) {
-    const feedback = feedbackMapper(req.body);
-    if (!feedback) {
+  const {
+    title,
+    location,
+    author,
+    address,
+    type,
+    password,
+    description,
+  } = req.body;
+
+  Feedback.create({
+    title,
+    location,
+    author,
+    address,
+    type,
+    password,
+    description,
+  }, (err, feedback) => {
+    if (err) {
       return res.status(500);
     }
-    feedback.save((err, data) => {
-      if (err) {
-        return res.status(500);
-      }
-      return res.status(201).json(data.toClient());
-    });
-  } else {
-    return res.status(400).json({ message: 'Required fields are not filled' });
-  }
+
+    return res.status(201).json(feedback.toClient());
+  });
 };
 
 module.exports.approveFeedback = (req, res) => res.status(403);
