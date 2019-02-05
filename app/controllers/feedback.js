657cbcccbf1c0cc1ee390085049a6476b57a9e39
Artem Dadychenko <a.dadychenko@mail.freebee.by>
2019-02-05 21:46:36 +0300

    Start developing

--- app/controllers/feedback.js
+++ app/controllers/feedback.js
@@ -1,4 +1,5 @@
 const Feedback = require('../models/feedback');
+const { freebeeTypesModels } = require('../utils/freebeeTypes');
 const { convertPointToGeoJSONPoint } = require('../utils/geo');
 
 module.exports.getFeedback = (req, res) => {
@@ -52,7 +53,22 @@ module.exports.createFeedback = (req, res) => {
   });
 };
 
-module.exports.approveFeedback = (req, res) => res.status(403);
+module.exports.approveFeedback = (req, res) => {
+  const { type } = req.body;
+
+  const MarkerModel = Object.values(freebeeTypesModels)
+    .find(freebeeType => freebeeType === type).model;
+
+  const marker = new MarkerModel({ ...req.body });
+  console.log('marker');
+  console.log(marker);
+
+  // marker.save((marker, error) => {
+
+  // });
+
+  return res.status(403);
+};
 
 module.exports.updateFeedback = (req, res) => {
   const {
