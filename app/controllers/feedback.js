47c824ec9b534bc53fae4b9034834dbf0958d349
Artem Dadychenko <a.dadychenko@mail.freebee.by>
2019-02-06 00:04:27 +0300

    Finish feedback approving implementation

--- app/controllers/feedback.js
+++ app/controllers/feedback.js
@@ -56,16 +56,31 @@ module.exports.createFeedback = (req, res) => {
 module.exports.approveFeedback = (req, res) => {
   const { type } = req.body;
 
-  const MarkerModel = Object.values(freebeeTypesModels)
-    .find(freebeeType => freebeeType === type).model;
+  if (!type || type.length === 0) {
+    return res.status(401);
+  }
+
+  const firstType = parseInt(type[0], 10);
+
+  // eslint-disable-next-line no-restricted-globals
+  if (isNaN(firstType)) {
+    return res.status(401);
+  }
+
+  const freebeeTypeWithModel = Object.values(freebeeTypesModels)
+    .find(typeModel => typeModel.type === firstType);
+
+  const MarkerModel = freebeeTypeWithModel.model;
 
   const marker = new MarkerModel({ ...req.body });
-  console.log('marker');
-  console.log(marker);
 
-  // marker.save((marker, error) => {
+  marker.save((createdMarker, err) => {
+    if (err) {
+      return res.status(500);
+    }
 
-  // });
+    return res.status(201).json(createdMarker.toClient());
+  });
 
   return res.status(403);
 };
