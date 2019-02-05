77fb6934babba0eeb689f56ff26675c214826c44
Artem Dadychenko <a.dadychenko@mail.freebee.by>
2019-02-06 00:05:48 +0300

    Revert "Start developing"
    This reverts commit 657cbcccbf1c0cc1ee390085049a6476b57a9e39.

--- app/utils/freebeeTypes.js
+++ app/utils/freebeeTypes.js
@@ -1,18 +1,5 @@
-const Toilet = require('../models/toilets');
-const Wifi = require('../models/wifi');
-
-const freebeeTypes = {
-  wifi: { value: 1, label: 'Wi-Fi' },
-  toilet: { value: 2, label: 'Туалет' },
-  socket: { value: 3, label: 'Розетка' },
+module.exports = {
+  wifi: { id: 1, value: 'Wi-Fi' },
+  toilet: { id: 2, value: 'Туалет' },
+  socket: { id: 3, value: 'Розетка' },
 };
-
-module.exports.freebeeTypes = freebeeTypes;
-
-const freebeeTypesModels = {
-  wifi: { type: freebeeTypes.wifi.value, model: Wifi },
-  toilet: { type: freebeeTypes.toilet.value, model: Toilet },
-  socket: { type: freebeeTypes.socket.value, model: null },
-};
-
-module.exports.freebeeTypesModels = freebeeTypesModels;
