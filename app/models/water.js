1e7a2e8e463efa162d003137ed87072775cfb919
Artem Dadychenko <a.dadychenko@mail.freebee.by>
2019-06-13 23:07:41 +0300

    Add water support

---
+++ app/models/water.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const pointSchema = require('./point');
+const { toClient } = require('./utils');
+
+mongoose.Promise = Promise;
+
+const water = new mongoose.Schema({
+  title: {
+    type: String,
+    required: false,
+    index: {
+      unique: false,
+    },
+  },
+  location: {
+    type: pointSchema,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: false,
+  },
+  creationDate: {
+    type: Date,
+    default: Date.now,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: false,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+}, {
+  collection: 'water',
+  versionKey: false,
+});
+
+water.method('toClient', toClient);
+
+module.exports = mongoose.model('water', water);
--- app/utils/freebeeTypes.js
+++ app/utils/freebeeTypes.js
@@ -1,10 +1,13 @@
 const Toilet = require('../models/toilets');
 const Wifi = require('../models/wifi');
+const Socket = require('../models/sockets');
+const Water = require('../models/water');
 
 const freebeeTypes = {
   wifi: { value: 1, label: 'Wi-Fi' },
   toilet: { value: 2, label: 'Туалет' },
   socket: { value: 3, label: 'Розетка' },
+  water: { value: 4, label: 'Вода' },
 };
 
 module.exports.freebeeTypes = freebeeTypes;
@@ -12,7 +15,8 @@ module.exports.freebeeTypes = freebeeTypes;
 const freebeeTypesModels = {
   wifi: { type: freebeeTypes.wifi.value, model: Wifi },
   toilet: { type: freebeeTypes.toilet.value, model: Toilet },
-  socket: { type: freebeeTypes.socket.value, model: null },
+  socket: { type: freebeeTypes.socket.value, model: Socket },
+  water: { type: freebeeTypes.water.value, model: Water },
 };
 
 module.exports.freebeeTypesModels = freebeeTypesModels;
